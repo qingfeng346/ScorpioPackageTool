3cdbbed5a0ad170b4e54c48e8455d6c20e516100
while <while@while.com>
2018-04-18 19:42:57 +0800

    修改生成脚本

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 set version=1.0.0
-rd /s /Q .\nodejs\apk
 electron-packager ./nodejs ScorpioAndroidTool --platform=win32 --overwrite --out ./bin --arch=all --version=%version%
 PAUSE
--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 version=1.0.0
-rm -rf ./nodejs/apk
 electron-packager ./nodejs ScorpioAndroidTool --platform=darwin --overwrite --out ./bin --arch=all --version=$version

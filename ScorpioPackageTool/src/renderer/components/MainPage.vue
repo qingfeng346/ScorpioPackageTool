e43680589c95fc1f890321bb8e15cd5b94b99d35
while <while@while.com>
2018-05-04 13:45:29 +0800

    update

---
+++ ScorpioPackageTool/src/renderer/components/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-container>
+        <el-aside width="200px"></el-aside>
+        <el-main>
+            <el-button type="primary" @click="TestClick()">efwefwaefwaefawef</el-button>
+        </el-main>
+    </el-container>
+</template>
+<script>
+    export default {
+        name:"main-page",
+        methods: {
+            TestClick : function() {
+                this.$message("wwwwwwwwwwww")
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
+
+

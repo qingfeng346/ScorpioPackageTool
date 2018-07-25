fe5724896f4566e5fe6d3ee8ae6110f1739f00dc
while <qingfeng346@fengyuezhu.com>
2018-07-25 23:56:28 +0800

    JavaInfo

--- JavaInfo/src/JavaInfo/Start.java
+++ JavaInfo/src/JavaInfo/Start.java
@@ -3,6 +3,9 @@ package JavaInfo;
 public class Start {
 	public static void main(String[] args) {
 		if (args.length <= 0) {
+			for (Object key : System.getProperties().keySet()) {
+				System.out.println(key + " = " + System.getProperty(key.toString()));
+			}
 			return;
 		}
 		System.out.println(System.getProperty(args[0]));
